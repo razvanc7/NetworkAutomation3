26db926ef5044f8d19caf49f5d9656f3591df49a
emcraciu <ozy24us@gmail.com>
2025-08-11 19:42:37 +0300

    modul3

--- modul3/prime.py
+++ modul3/prime.py
@@ -16,6 +16,34 @@ def prime():
 
     return result_in_function
 
+
+# returnati primele 100 de numere prime
+print(50 * "*")
+
+def is_prime(num):
+    if num < 2:
+        return False
+    if num == 2:
+        return True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def first_100_primes():
+    primes = []
+    j = 0
+    while len(primes) < 100:
+        if is_prime(j):
+            primes.append(j)
+        j += 1
+    return primes
+
+
+print(primes_100 := first_100_primes())
+
+# print(__name__)
 if __name__ == '__main__':
     result = prime()
     print(result)
