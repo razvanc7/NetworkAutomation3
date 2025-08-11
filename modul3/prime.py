7c6dd1d3e7fd09553dc6dca8c244b06fa03fca01
emcraciu <ozy24us@gmail.com>
2025-08-11 19:32:28 +0300

    modul3

--- modul3/prime.py
+++ modul3/prime.py
@@ -1,1 +1,21 @@
 # function for first 100 prime numbers
+
+def prime():
+    prime_counter = 0
+    nrs = 3
+    result_in_function = [1, 2]
+
+    while (prime_counter <= 100):
+        for i in range(2, nrs // 2 + 1):
+            if not nrs % i:
+                break
+        else:
+            prime_counter += 1
+            result_in_function.append(nrs)
+        nrs += 1
+
+    return result_in_function
+
+if __name__ == '__main__':
+    result = prime()
+    print(result)
