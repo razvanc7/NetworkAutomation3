cb0d90b6454bf76f1dfd72c3cf7f09f1b873ebc5
emcraciu <ozy24us@gmail.com>
2025-08-12 19:02:25 +0300

    modul4

--- modul4/telnet_connection.py
+++ modul4/telnet_connection.py
@@ -1,7 +1,16 @@
+import asyncio
+
 import telnetlib3
 HOST = '92.81.55.146'
 PORT = 5041
 
 async def connect_to_device(host, port):
     reader, writer = await telnetlib3.open_connection(host, port)
+    writer.write('\n')
+    response = await asyncio.wait_for(reader.read(200), timeout=1)
+    print(response)
+    # writer.close()
+    # await writer.wait_closed()
+
+asyncio.run(connect_to_device(HOST, PORT))
 
