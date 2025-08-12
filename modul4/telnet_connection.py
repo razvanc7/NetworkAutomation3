25cf9ac72d0420368aa0daaa6ecbb46341cff6f0
emcraciu <ozy24us@gmail.com>
2025-08-12 19:35:39 +0300

    modul4

--- modul4/telnet_connection.py
+++ modul4/telnet_connection.py
@@ -7,8 +7,15 @@ PORT = 5041
 async def connect_to_device(host, port):
     reader, writer = await telnetlib3.open_connection(host, port)
     writer.write('\n')
-    response = await asyncio.wait_for(reader.read(200), timeout=1)
+    response = await reader.readuntil(b"IOU1#")
+    # response = await asyncio.wait_for(reader.read(200), timeout=2)
     print(response)
+    if 'IOU1#' in response.decode():
+        writer.write("show interfaces Ethernet1/1\n ")
+        response = await  reader.readuntil(b"IOU1#")
+
+        # print(type(response))
+        # print(response)
     # writer.close()
     # await writer.wait_closed()
 
---
+++ modul4/work_with_bytes.py
@@ -0,0 +1,5 @@
+b = b"byte string"
+print(type(b))
+print(b)
+print(b.decode())
+print("string".encode())
