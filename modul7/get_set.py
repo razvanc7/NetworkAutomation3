2588b1461e9126b1935648da34cb578ae26dfa98
emcraciu <ozy24us@gmail.com>
2025-08-20 20:01:32 +0300

    modul7

--- modul7/get_set.py
+++ modul7/get_set.py
@@ -1,8 +1,21 @@
-class Triangle:
+class Triangle(object):
     def __init__(self, a: int, b: int, c: int):
         self.a = a
         self.b = b
         self.c = c
+
+    def __setattr__(self, key, value):
+        # x = getattr(self, key, None)
+        super().__setattr__(key, value + 1) # change assigned value
+        # print(dir(self))
+        # print(key)
+
+    def __delattr__(self, key):
+            super().__delattr__(key)
+
+    def __getattribute__(self, key):
+        return super().__getattribute__(key)
+
 
 t = Triangle(1, 1, 1)
 
@@ -13,5 +26,14 @@ print(type(key), type(value))
 # if key == 'a':
 #     t.a = 2
 
+t.__setattr__('example', 'value')
+
 setattr(t, key, value)
+# t.__setattr__(key, value)
 print(getattr(t, key))
+# print(t.__getattribute__(key))
+
+print(t.a)
+delattr(t, key)
+# t.__delattr__(key)
+print(t.a)
