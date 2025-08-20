9f681bb9ca3399bd0f5fc0314935c03c19cfcbfb
emcraciu <ozy24us@gmail.com>
2025-08-20 19:02:21 +0300

    modul7

---
+++ modul7/ex3.py
@@ -0,0 +1,11 @@
+# create list of functions where function is x + Y and Y is incremented from 1 to 100
+
+list_of_func = []
+for number in range(1, 101):
+    func = lambda x: x + number # fix variable change in function !!
+    list_of_func.append(func)
+
+print(list_of_func)
+
+print(list_of_func[0](2))
+print(list_of_func[1](2))
