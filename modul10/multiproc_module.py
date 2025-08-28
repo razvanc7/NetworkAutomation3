517e0724c730cdd49fedcbd8ff8e3e31750b0fcd
emcraciu <ozy24us@gmail.com>
2025-08-28 19:37:30 +0300

    module 8

--- modul10/multiproc_module.py
+++ modul10/multiproc_module.py
@@ -24,18 +24,47 @@ import time
 
 
 
-from multiprocessing import Pool
+# from multiprocessing import Pool
+#
+# def square(x):
+#     print(f'{os.getpid()} calculating square of {x}')
+#     time.sleep(3)
+#
+#     return x * x
+#
+# if __name__ == "__main__":
+#     start = time.time()
+#     with Pool(4) as pool:
+#         results = pool.map(square, [1, 2, 3, 4, 5])
+#         print(results)
+#     end = time.time()
+#     print(end - start)
 
-def square(x):
-    print(f'{os.getpid()} calculating square of {x}')
-    time.sleep(3)
 
-    return x * x
+from multiprocessing import Process, Queue
+
+def producer(q):
+    for i in range(5):
+        time.sleep(1)
+        q.put(i)
+
+def consumer(q):
+    while not q.empty():
+        item = q.get()
+        print("Consuming:", item)
+        time.sleep(2) # determines consumtion rate
+        print("Consumed:", item)
+    print(f"Consumer {os.getpid()} done")
 
 if __name__ == "__main__":
-    start = time.time()
-    with Pool(4) as pool:
-        results = pool.map(square, [1, 2, 3, 4, 5])
-        print(results)
-    end = time.time()
-    print(end - start)
+    q = Queue()
+    p1 = Process(target=producer, args=(q,))
+    p2 = Process(target=consumer, args=(q,))
+    p3 = Process(target=consumer, args=(q,))
+    p1.start()
+    time.sleep(3)
+    p2.start()
+    p3.start()
+    p1.join()
+    p2.join()
+    p3.join()
