2917a44277a50664824fe99412ce3ec62d28e483
emcraciu <emcraciu@cisco.com>
2025-09-11 19:52:01 +0300

    modul 13

--- modul14/configure_genie.py
+++ modul14/configure_genie.py
@@ -1,6 +1,8 @@
+
 from pyats import aetest, topology
 from genie.libs.conf.interface.iosxe import Interface
-from pyats.topology import Testbed
+from genie.libs.conf.static_routing import StaticRouting
+from pyats.topology import Device
 
 
 class CommonSetup(aetest.CommonSetup):
@@ -13,11 +15,42 @@ class CommonSetup(aetest.CommonSetup):
 
 class ConfigureGenie(aetest.Testcase):
     @aetest.setup
-    def test1(self, steps):
-        tb: Testbed = self.parent.parameters.get("tb")
-        dev = tb.devices.CSR
-        dev.connect(log_stdout=True)
-        print(dev)
+    def connect(self, steps):
+        tb = self.parent.parameters.get("tb")
+        self.dev: Device = tb.devices.CSR
+        self.dev.connect(log_stdout=True)
+
+    @aetest.test
+    def configure_interfaces(self, steps):
+        with steps.start("Configure interface 1"):
+            intf = Interface(
+                name='GigabitEthernet2'
+            )
+            intf.device = self.dev
+            intf.ipv4 = self.dev.interfaces['GigabitEthernet2'].ipv4.ip
+            config = intf.build_config(apply=False)
+            self.dev.configure(config.cli_config.data)
+            print(config)
+
+        with steps.start("Configure interface 2"):
+            intf = Interface(
+                name='GigabitEthernet3'
+            )
+            intf.device = self.dev
+            intf.ipv4 = self.dev.interfaces['GigabitEthernet3'].ipv4.ip
+            config = intf.build_config(apply=False)
+            self.dev.configure(config.cli_config.data)
+            print(config)
+
+        with steps.start("Configure static routing"):
+            route = StaticRouting()
+            route.device = self.dev
+            route.route =
+            config = route.build_config(apply=False)
+            self.dev.configure(config.cli_config.data)
+
+
+
 
 if __name__ == '__main__':
     aetest.main()
