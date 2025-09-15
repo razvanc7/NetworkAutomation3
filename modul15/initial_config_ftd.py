69792d1c392243e991063ce393c3dc11d8d79e54
emcraciu <emcraciu@cisco.com>
2025-09-15 21:14:32 +0300

    modul 15

--- modul15/initial_config_ftd.py
+++ modul15/initial_config_ftd.py
@@ -1,11 +1,10 @@
 import asyncio
-import subprocess
+import re
 import sys
 import time
 
 from pyats import aetest, topology
 
-import modul12.commands as ssh_commands
 from lib.connectors.telnet_con import TelnetConnection
 
 print(sys.path)
@@ -17,7 +16,6 @@ class CommonSetup(aetest.CommonSetup):
         with steps.start("Load testbed"):
             self.tb = topology.loader.load('testbed.yaml')
             self.parent.parameters.update(tb=self.tb)
-
 
     @aetest.subsection
     def bring_up_router_interface(self, steps):
@@ -43,11 +41,86 @@ class CommonSetup(aetest.CommonSetup):
                         conn.write('')
                         time.sleep(1)
                         out = await conn.read(n=1000)
+                        print(out)
+                        result = re.search(r'^(?P<login>firepower login:)', out)
+                        if result.group('login'):
+                            conn.write('admin')
+                            time.sleep(0.1)
+                            conn.write('Admin123')
+                            time.sleep(1)
 
+                        out = await conn.read(n=1000)
+                        if 'EULA:' in out:
+                            conn.write('\n')
+
+                            while True:
+                                time.sleep(1)
+                                out = await conn.read(n=1000)
+                                if '--More--' in out:
+                                    conn.write(' ')
+                                elif 'EULA:' in out:
+                                    conn.write('\n')
+                                    time.sleep(1)
+                                    out = await conn.read(n=1000)
+                                    break
+                                else:
+                                    print('no str found in eula')
+
+                        if 'password:' in out:
+                            conn.write(self.tb.devices[device].credentials.default.password.plaintext)
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+                            if 'password:' in out:
+                                conn.write(self.tb.devices[device].credentials.default.password.plaintext)
+                                time.sleep(1)
+                                out = await conn.read(n=1000)
+
+                        if 'IPv4? (y/n) [y]:' in out:
+                            conn.write('')
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if 'IPv6? (y/n) [n]:' in out:
+                            conn.write('')
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if '[manual]:' in out:
+                            conn.write('')
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if '[192.168.45.45]:' in out:
+                            conn.write(intf_obj.ipv4.ip.compressed)
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if '[255.255.255.0]:' in out:
+                            conn.write(intf_obj.ipv4.netmask.exploded)
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if '[192.168.45.1]:' in out:
+                            conn.write((intf_obj.ipv4.ip + 1).compressed)
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if '::35]:' in out:
+                            conn.write('')
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if "'none' []:" in out:
+                            conn.write('')
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
+
+                        if "locally? (yes/no) [yes]:" in out:
+                            conn.write('')
+                            time.sleep(1)
+                            out = await conn.read(n=1000)
 
                     asyncio.run(setup())
-
-
 
 
 class ConfigureInterfaces(aetest.Testcase):
