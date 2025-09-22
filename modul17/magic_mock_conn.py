872b22f667be87ab8a3fcb85eae69227a0d23d32
emcraciu <emcraciu@cisco.com>
2025-09-22 20:41:30 +0300

    modul 16

--- modul17/magic_mock_conn.py
+++ modul17/magic_mock_conn.py
@@ -11,3 +11,17 @@ class TestCase(unittest.TestCase):
         conn = RESTConnector('10.10.10.10', 8888, 'user1', 'password')
         conn.connect()
         self.assertEqual({"hardwareName": 'Ethernet3'}, conn.get_interface('Ethernet3'))
+
+    @patch('lib.connectors.rest_conn.get')
+    def test_get_restconf_capabilities(self, requests_mock):
+        requests_mock.return_value = MagicMock(json=MagicMock(return_value={
+            'ietf-yang-library:modules-state': {
+                'module': [{
+                    'schema': 'htpp://myserver.com/myapiendpoint'
+                }]
+            }
+        }))
+        from lib.connectors.rest_con import RESTConnector
+        conn = RESTConnector('10.10.10.10', 8888, 'user1', 'password')
+        conn.connect()
+        self.assertEqual(['htpp://myserver.com/myapiendpoint'], conn.get_restconf_capabilities())
