5e69e6a65ad25314ea6601396a8f3694922a2c85
emcraciu <emcraciu@cisco.com>
2025-09-25 21:50:20 +0300

    modul 18

--- modul18/ansible_runner_playbook.py
+++ modul18/ansible_runner_playbook.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 import ansible_runner
+
+BASE = Path(__file__).parent.resolve()
+PLAYBOOK = str((BASE / "add_static_route.yaml").resolve())
+INVENTORY = str((BASE / "inventory.ini").resolve())
 
 out = ansible_runner.run(
     private_data_dir='.',
-    playbook='add_static_route.yaml',
-    inventory='inventory.ini',
+    playbook=PLAYBOOK,
+    inventory=INVENTORY,
     json_mode=True,
 )
 print(out)
